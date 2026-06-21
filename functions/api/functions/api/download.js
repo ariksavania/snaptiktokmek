7534e09c8269eab2d9f38fb5aaf05a2bb135b698
ariksavania <162962305+ariksavania@users.noreply.github.com>
2026-06-21 09:22:04 +0700

    Delete functions/api/functions/api directory

--- functions/api/functions/api/download.js
+++
@@ -1,8 +0,0 @@
-export async function onRequestPost(context) {
-  return new Response(JSON.stringify({ 
-    status: "success", 
-    message: "API Berhasil terhubung!" 
-  }), {
-    headers: { "Content-Type": "application/json" }
-  });
-}
